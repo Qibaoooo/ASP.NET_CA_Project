5b257bf81bd34be82f46f82e870ff35c9a53bbeb
Qibaoooo <luozhengiw@gmail.com>
2023-10-05 19:08:23 +0800

    init

---
+++ ASP.NET_CA_Project/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ASP.NET_CA_Project.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
