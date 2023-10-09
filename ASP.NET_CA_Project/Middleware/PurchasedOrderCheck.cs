a0b9c7df34ef5163dd8d43f5368618832b973013
calvinpoh98 <calvinpoh01@gmail.com>
2023-10-09 14:48:27 +0800

    save before rebase

---
+++ ASP.NET_CA_Project/Middleware/PurchasedOrderCheck.cs
@@ -0,0 +1,16 @@
+﻿namespace ASP.NET_CA_Project.Middleware
+{
+    public class PurchasedOrderCheck
+    {
+        private RequestDelegate next;
+
+        public PurchasedOrderCheck(RequestDelegate next){
+            this.next = next;
+        }
+
+        public async Task Invoke(HttpContext context) {
+            ISession session = context.Session;
+            await next(context);
+        }
+    }
+}
