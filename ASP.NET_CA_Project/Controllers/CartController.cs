033df625ba600da799248c852f322992d0f2f1c6
Inella12138 <141432262+Inella12138@users.noreply.github.com>
2023-10-12 11:05:17 +0800

    Still in trouble TAT

--- ASP.NET_CA_Project/Controllers/CartController.cs
+++ ASP.NET_CA_Project/Controllers/CartController.cs
@@ -1,14 +1,42 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ASP.NET_CA_Project.Database;
+using ASP.NET_CA_Project.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace ASP.NET_CA_Project.Controllers
 {
-    public class CartController : Controller
+    public class CartController : BaseController
     {
+        public CartController(ShopDBContext db):base(db)
+        { }
         // GET: /<controller>/
-        public IActionResult Index()
+        public IActionResult Index(Guid? userID)
         {
+            List<Order> allOrders = (List<Order>)db.Order.ToList();
+            List<Item> orderItems = (from order in allOrders
+                                    select order.Item).ToList();
+
+            /*var userorder = from order in allOrders
+                            where order.User.Id == userID
+                            select order;*/
+
+            /*var orderItems = from order in userorder
+                             group order by order.Item.Id into itemgroup
+                             select new
+                             {
+                                 ItemName = itemgroup.First().Item.ItemName,
+                                 Quantity = itemgroup.First().Count,
+                                 Price = itemgroup.First().Item.Price * itemgroup.First().Count
+                             };*/
+
+            ViewBag.Orders = allOrders;
+            ViewBag.orderItems = orderItems;
+
             return View();
         }
 
