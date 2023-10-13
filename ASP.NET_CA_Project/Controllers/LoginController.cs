3209915e4c0328a5f308dffdf98a6adc552bddbe
calvinpoh98 <calvinpoh01@gmail.com>
2023-10-13 20:41:10 +0800

    Update controller and js

--- ASP.NET_CA_Project/Controllers/LoginController.cs
+++ ASP.NET_CA_Project/Controllers/LoginController.cs
@@ -3,17 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ASP.NET_CA_Project.Database;
+using ASP.NET_CA_Project.Models;
+using Microsoft.AspNetCore.Identity;
+
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace ASP.NET_CA_Project.Controllers
 {
-    public class LoginController : Controller
+    public class LoginController : BaseController
     {
+        public LoginController(ShopDBContext db) : base(db) { }
         // GET: /<controller>/
         public IActionResult Index(bool isLoggedIn)
         {
             ViewData["isLoggedIn"] = isLoggedIn;
+            return View();
+        }
+
+        public IActionResult AuthenticateUser(string username, string password) {
+            //Check if user is in database
+            User? user = db.User.FirstOrDefault(x => x.UserName == username);
+            //Check if session has an ongoing cart and user has an existing cart
+
+            if (user == null)
+            {
+                return Json(new { }); //User not valid, please try again rendered into login html
+            }
+            else if (user != null && user.Password == password)
+            { 
+                return Json(new { }); //Return success and redirect
+            }
             return View();
         }
 
